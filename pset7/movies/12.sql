6630a38c8110271057c485f1761b9a14282ae40b
Adele Gikonyo <53218392+adelewg@users.noreply.github.com>
2020-10-26 14:50:29 +0300

    Create 12.sql

---
+++ pset7/movies/12.sql
@@ -0,0 +1,4 @@
+SELECT title from movies
+WHERE id IN (SELECT movie_id FROM stars
+WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp")
+and movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Helena Bonham Carter")));
